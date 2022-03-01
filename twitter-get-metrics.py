71491da0abc382009810f4e301b05761e4a3c4ba
Egger Haaaaan <bboyhan@hotmail.com>
2022-02-28 21:27:18 -0500

    Create twitter-get-metrics.py

---
+++ twitter-get-metrics.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+import os
+import requests
+from requests_oauthlib import OAuth1
+
+# Argparse for cli options. Run `python engagement_totals.py -h` to see list of available arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--tweet_ids", nargs='+', required=True,
+                    help="Enter one or more comma delimited Tweet IDs (for example: `-t '123,456'`)")
+
+args = parser.parse_args()
+
+# Retrieves and stores credential information from the '.env' file
+CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# Generate user context auth (OAuth1)
+user_context_auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, TOKEN_SECRET)
+
+# Totals API endpoint (same for all accounts)
+endpoint = f"https://api.twitter.com/2/tweets/{args.tweet_ids[0]}?tweet.fields=non_public_metrics,organic_metrics&media.fields=non_public_metrics,organic_metrics&expansions=attachments.media_keys"
+
+headers = {"Accept-Encoding": "gzip"}
+
+response = requests.get(endpoint, auth=user_context_auth, headers=headers)
+
+parsed = json.loads(response.text)
+pretty_print = json.dumps(parsed, indent=2, sort_keys=True)
+
+print (pretty_print)
