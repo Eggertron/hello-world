56a83e3e6c7c91eac6dde976a88005a1ae5a6396
Egger Haaaaan <bboyhan@hotmail.com>
2022-02-28 22:05:38 -0500

    Update twitter-get-metrics.py

--- twitter-get-metrics.py
+++ twitter-get-metrics.py
@@ -13,12 +13,14 @@ user_context_auth = OAuth1(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTE
 
 # Totals API endpoint (same for all accounts)
 endpoint = f"https://api.twitter.com/2/tweets/{TWEET_ID}?tweet.fields=non_public_metrics,organic_metrics&media.fields=non_public_metrics,organic_metrics&expansions=attachments.media_keys"
+print(f"endpoint: {endpoint}")
 
 headers = {"Accept-Encoding": "gzip"}
+print(f"headers:{headers}")
 
 response = requests.get(endpoint, auth=user_context_auth, headers=headers)
 
 parsed = json.loads(response.text)
 pretty_print = json.dumps(parsed, indent=4, sort_keys=True)
 
-print (pretty_print)
+print(pretty_print)
